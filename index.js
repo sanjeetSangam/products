c344c820ce958e99c98dabda800ed420b6aa9137
Sanjeet Kumar Sangam <93376461+sanjeetSangam@users.noreply.github.com>
2022-06-29 12:52:53 +0530

    updated with comments

--- index.js
+++ index.js
@@ -1,80 +1,85 @@
-const listOfProducts = [{
+// data
+const listOfProducts = [
+  {
+    productName: "TV",
+    quantity: 10,
+    description: "television",
+  },
+  {
+    productName: "AC",
+    quantity: 5,
+    description: "air conditioner",
+  },
+  {
+    productName: "TV",
+    quantity: 10,
+    description: "television",
+  },
+  {
+    productName: "AC",
+    quantity: 5,
+    description: "air conditioner",
+  },
+  {
+    productName: "FAN",
+    quantity: 10,
+    description: "Ceiling Fan",
+  },
+];
 
-productName: "TV",
+// 1.make unique count of products ---------------------------------
+function getUniqueProductCount(listOfProducts) {
+  // empty object for storing unique value
+  let productsCount = {};
 
-quantity: 10,
-
-description: "television"
-},
-
-{
-
-productName: "AC",
-
-quantity: 5,
-
-description: "air conditioner"
-
-},
-
-{
-
-productName: "TV",
-
-quantity: 10,
-
-description: "television"
-},
-
-{
-
-productName: "AC",
-
-quantity: 5,
-
-description: "air conditioner"
-
-},
-
-{
-productName : "FAN",
-quantity: 10,
-description : "Ceiling Fan"
-
-}
-]  
-
-
-function getUniqueProductCount(listOfProducts){
-    let uniqueCount = {};
-    
-    for (let item of listOfProducts) {
-        if(uniqueCount[item.productName] === undefined){
-            uniqueCount[item.productName] = 1;
-        } else {
-            uniqueCount[item.productName]= uniqueCount[item.productName] +1;
-        }
-   
-    }
-    
-   console.log(uniqueCount);
-}
-
-getUniqueProductCount(listOfProducts)
-
-function getUniqueProducts (products){
-    var obj = {};
-    
-    products.forEach((e) => {
-    if (obj[e.productName] == undefined) {
-      obj[e.productName] = e;
+  // loop for inserting in productsCount
+  for (let item of listOfProducts) {
+    if (productsCount[item.productName] === undefined) {
+      productsCount[item.productName] = 1;
     } else {
-      obj[e.productName].quantity += e.quantity;
+      productsCount[item.productName] = productsCount[item.productName] + 1;
+    }
+  }
+
+  return productsCount;
+}
+
+//printing value of unique count
+let counts = getUniqueProductCount(listOfProducts);
+console.log(counts);
+
+// 2. function for making sum of same prodcuts at one place ---------------------------------------------------
+function getUniquePrducts(listOfProducts) {
+  // empty array for storing values
+  let productTotalQuantity = [];
+
+  for (let item of listOfProducts) {
+    if (search(productTotalQuantity, item) === true) {
+      listOfProducts.find((prod) => {
+        if (prod.productName === item.productName) {
+          prod.quantity += item.quantity;
+        }
+      });
+    } else {
+      productTotalQuantity.push(item);
+    }
+  }
+  return productTotalQuantity;
+}
+
+// for finding the value is present or not
+function search(arr, item) {
+  let r = false;
+
+  let res = arr.find((o) => {
+    if (o.productName === item.productName) {
+      r = true;
+      return;
     }
   });
-  
-  return Object.values(obj);
+
+  return r;
 }
 
-let unique = getUniqueProducts(listOfProducts);
-console.log(unique);
+let result = getUniquePrducts(listOfProducts);
+console.log(result);
